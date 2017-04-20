f4fb52b2af31bc93632104957ccb84ce6ca31cf9
Nizam <nizamudeeen@gmail.com>
2017-04-20 17:09:44 +0530

    added calss

---
+++ Test/src/com/test/nizam/SayHello.java
@@ -0,0 +1,11 @@
+package com.test.nizam;
+
+public class SayHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Hello");
+
+	}
+
+}
